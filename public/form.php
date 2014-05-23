9ad85c7a4a66a36f8d1cf3d0acb27f70c14f9b66
Andre Dempsey <andre.dempsey@live.com>
2014-05-23 08:27:03 -0500

    revised form to add placeholders and experiment with different submit button

--- public/form.php
+++ public/form.php
@@ -9,17 +9,20 @@
 	var_dump($_GET);
 	var_dump($_POST);
 	?>
-	<form method="GET" action="/form.php">
+	<form method="POST" action="/form.php">
     <p>
         <label for="username">Username</label>
-        <input id="username" name="username" type="text">
+        <input id="username" name="username" type="text" placeholder="username">
+        <!-- alternative formatting -->
+        <!-- <label>Username: <input name="username" type ="text"></label> -->
     </p>
     <p>
         <label for="password">Password</label>
-        <input id="password" name="password" type="password">
+        <input id="password" name="password" type="password" placeholder="password">
     </p>
     <p>
-        <input type="submit">
+        <!-- <input type="submit" name="submitbutton" value = "Login"> -->
+        <button type = "Submit">Login</button>
     </p>
 </form>	
 </body>
