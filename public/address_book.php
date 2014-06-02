6b950833064fa9b1c04bcf6f58bfd01749a81627
Andre Dempsey <andre.dempsey@live.com>
2014-06-02 15:29:06 -0500

    added function to add address to CSV file after validating fields are not blank

--- public/address_book.php
+++ public/address_book.php
@@ -1,19 +1,31 @@
 <?
-
-//sample data
-$address_book = [
-    ['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
-    ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
-    ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
-];
-
-$handle = fopen('address_book.csv', 'w');
-
-foreach ($address_book as $fields) {
-    fputcsv($handle, $fields);
+$filename = 'address_book.csv';
+$addressBook =[];
+$errorMessage='';
+function addAddress($filename, $addressBook)
+{
+	$handle = fopen($filename, 'w');
+	fputcsv($handle, $addressBook);
+	fclose($handle);
+	return $addressBook;
 }
-
-fclose($handle);
+if (!empty($_POST)) 
+{
+	$missingValues=false;
+	foreach ($_POST as $key=>$entry) 
+	{
+		$_POST[$key]=htmlspecialchars(strip_tags($entry));
+		if (empty($entry)&&$key!='Phone')
+		{
+			$errorMessage .= "Please enter a value for the $key field.\n<br>";
+			$missingValues = true;
+		}		
+	}
+		if (!$missingValues) 
+		{
+			$addressBook=addAddress($filename, $_POST);
+		}
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -31,31 +43,32 @@ fclose($handle);
 		        <th>City</th>
 		        <th>State</th>
 				<th>Zip Code</th>
+				<th>Phone</th>
 		    </tr>
-			<? foreach ($address_book as $key=>$address) :?>
 		    <tr>
-				<? foreach ($address as $element=>$item) :?>
+				<? foreach ($addressBook as $element=>$item) :?>
 		        <td><?=$item;?></td>
 				 <?endforeach; ?>
  		    </tr>
-			<?endforeach; ?>
 	</table>
 <hr>
 	<form method="POST">
-        <p>
-            <label for="addressName">Name:</label>
-            <input id="addressName" name="addressName" type="text" placeholder="Enter Name">
-            <label for="addressAddress">Street Address:</label>
-            <input  id="addressAddress" name="addressAddress" type="text" placeholder="Enter Street Address">
+		<p>
+            <label for="Name">Name:</label>
+            <input id="Name" name="Name" type="text" placeholder="Enter Name">
+            <label for="Address">Street Address:</label>
+            <input  id="Address" name="Street Address" type="text" placeholder="Enter Street Address">
             <label for="addressCity">City:</label>
-            <input  id="addressCity" name="addressCity" type="text" placeholder="Enter City">
+            <input  id="addressCity" name="City" type="text" placeholder="Enter City">
             <label for="addressState">State:</label>
-            <input  id="addressState" name="addressState" type="text" placeholder="Enter State">
+            <input  id="addressState" name="State" type="text" placeholder="Enter State">
             <label for="addressZip">Zip Code:</label>
-            <input  id="addressZip" name="addressZip" type="text" placeholder="Enter Zip Code">
-     
+            <input  id="addressZip" name="Zip" type="text" placeholder="Enter Zip Code">
+            <label for="addressPhone">Phone Number:</label>
+            <input  id="addressPhone" name="Phone" type="text" placeholder="Enter Phone Number">
+		</p>
 	<input type="submit" value="Add Address">
 	</form>
+	<?= $errorMessage;?>
 </body>
-<? var_dump($_POST); ?>
 </html>
