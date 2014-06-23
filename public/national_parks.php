d9441c0529ddcd0399a611b95375f6ba169246a2
Andre Dempsey <andre.dempsey@live.com>
2014-06-23 11:20:00 -0500

    refactored to only show 4 parks and added links for previous/next page-not yet implemented

--- public/national_parks.php
+++ public/national_parks.php
@@ -9,7 +9,13 @@ $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
-$query = "SELECT * FROM national_parks";
+$numRecords = 4;
+$pageNumber= 0;
+// function buildQuery($records=)
+// {
+// 	# code...
+// }
+$query = "SELECT * FROM national_parks LIMIT {$numRecords} OFFSET {$pageNumber}";
 
 $stmt = $dbc->query($query);
 $parks = $stmt->fetchAll(PDO::FETCH_NUM);
@@ -25,13 +31,13 @@ $results = $stmt->rowCount();
 </head>
 <body>
 	<p>Total number of results: <?=$results?></p>
-	<table border =1">
+	<table border = "1">
 	    <tr>
 	        <th>id</th>
 	        <th>Name</th>
 	        <th>Location</th>
-	        <th>Date</th>
-	        <th>Acres</th>
+	        <th>Date Established</th>
+	        <th>Size (Acres)</th>
 	    </tr>
 	<?foreach ($parks as $park) :?>
 		<tr>
@@ -41,5 +47,9 @@ $results = $stmt->rowCount();
 		</tr>
 	<?endforeach;?>
 	</table>
+	<form method="GET" action="/national_parks.php">
+		<a href="">Previous Page</a>
+		<a href="">Next Page</a>
+	</form>
 </body>
 </html>
