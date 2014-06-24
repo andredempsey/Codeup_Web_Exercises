8f7d7d64b446ab3cd42c156098d0f7a5364dfb97
Andre Dempsey <andre.dempsey@live.com>
2014-06-23 19:03:06 -0500

    user can add new parks; input checked for null values

--- public/national_parks.php
+++ public/national_parks.php
@@ -8,10 +8,64 @@ $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
+$errorMessage='';
 $numRecords = 4;
 $offsetValue = 0;
 $pageNumber= 1;
+class InvalidInputException extends Exception {}
 
+//checks if field is blank and updates error message
+function checkFields($parkEntries, &$errorMessage)
+{
+	$missingValues=false;
+	foreach ($parkEntries as $key=>$entry) 
+	{
+		if (empty($entry) && $key != 'Description')
+		{
+			$errorMessage .= "Please enter a value for the $key field.\n<br>";
+			$missingValues = true;
+		}		
+	}
+	return $missingValues;
+}
+//checks if field has space and throws error
+function inputValidation($inputValue, $key)
+{
+
+	if (trim($inputValue) == '')
+	{
+		throw new InvalidInputException($key . ' input cannot be null.');
+	}
+	else
+	{
+		return trim($inputValue);
+	}
+}
+//if park was submitted, insert into database using prepare statements
+if (!empty($_POST)) 
+{
+	if (!checkFields($_POST,$errorMessage)) 
+	{
+		try {
+			$stmt = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_est, :acres, :desc)');
+		    $stmt->bindValue(':name', inputValidation($_POST['Name'],'name'), PDO::PARAM_STR);
+		    $stmt->bindValue(':location', inputValidation($_POST['Location'],'location'),  PDO::PARAM_STR);
+		    $stmt->bindValue(':date_est', inputValidation($_POST['DateEst'], 'date_est'), PDO::PARAM_STR);
+		    $stmt->bindValue(':acres', inputValidation($_POST['Size'], 'acres'), PDO::PARAM_STR);
+		    $stmt->bindValue(':desc',  $_POST['Description'],  PDO::PARAM_STR);
+		    $stmt->execute();
+		 	$errorMessage = "Inserted ID: " . $dbc->lastInsertId();
+			$_POST=[];
+			
+		} 
+		catch (Exception $e) 
+		{
+			$errorMessage=$e->getMessage();
+		}
+	}
+}
+
+//if page was changed, update data set using prepare statements and SQL SELECT query
 if (isset($_GET['Page']))
 {
 	if (isset($_GET['Nav']))
@@ -35,18 +89,18 @@ if (isset($_GET['Page']))
 }
 
 $query = "SELECT * FROM national_parks LIMIT :numRecs OFFSET :offsetVal";
-
 $stmt = $dbc->prepare($query);
-
 $stmt->bindValue(':numRecs', $numRecords, PDO::PARAM_INT);
 $stmt->bindValue(':offsetVal', $offsetValue, PDO::PARAM_INT);
 $stmt->execute();
-
 $parks = $stmt->fetchAll(PDO::FETCH_NUM);
 
+//determine count of records returned
 $results = $stmt->rowCount();
+//determine total pages for entire data set
 $totalPages = ($dbc->query('SELECT * FROM national_parks')->rowCount()/$numRecords);
 
+//if no results from query, do not move to next page
 if ($results == 0) 
 {
 	$pageNumber = $_GET['Page'];
@@ -58,9 +112,7 @@ if ($results == 0)
 	$stmt->execute();
 	$parks = $stmt->fetchAll(PDO::FETCH_NUM);
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -98,7 +150,28 @@ if ($results == 0)
 		</ul>
 		</div>
 	</form>
+	<p style="text-align: center">Page <?=$pageNumber?> of <?=ceil($totalPages)?> pages.</p>
 	<p style="text-align: center">You are viewing <?=$results?> of <?=$totalPages*$numRecords?> total results.</p>
+	</div>
+	<p style="text-align: center"><font color="DC143C"><?= (is_null($errorMessage))?"":$errorMessage; ?></font></p>
+	<hr>
+	<div class = "container">
+		<form method="POST" action="/national_parks.php">
+			<p>
+	            <label for="Name">Park Name:</label>
+	            <input id="Name" name="Name" type="text" value=<?=(!empty($_POST) && $_POST['Name']!='')?$_POST['Name']:'' ?> >
+	            <label for="Location">Park Location:</label>
+	            <input  id="Location" name="Location" type="text" value=<?=(!empty($_POST) && $_POST['Location']!='')?$_POST['Location']:'' ?> >
+	            <label for="DateEst">Date Established:</label>
+	            <input  id="DateEst" name="DateEst" type="text" value=<?=(!empty($_POST) && $_POST['DateEst']!='')?$_POST['DateEst']:'' ?> >
+	            <label for="Size">Size (Acres):</label>
+	            <input  id="Size" name="Size" type="text" value=<?=(!empty($_POST) && $_POST['Size']!='')?$_POST['Size']:'' ?> >
+	            <div><label for="Description">Description:</label>
+		            <input  id="Description" name="Description" type="text" placeholder="Optional">
+	            </div>
+			</p>
+		<input type="submit" value="Add Park">
+		</form>
 	</div>
 </body>
 </html>
